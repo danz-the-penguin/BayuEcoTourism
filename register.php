965f6c3c4d986c4c19d80ea3357bf64ede24618a
Dan M <dansresearcheaddrss@gmail.com>
2025-01-18 17:16:09 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+$conn = new mysqli("localhost", "username", "password", "database");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO Users (username, password, email) VALUES ('$username', '$password', '$email')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+$conn->close();
+?>
