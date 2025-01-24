7ead570c24271404320228bc28041e342c0e383b
Dan M <dansresearcheaddrss@gmail.com>
2025-01-24 22:53:06 +0800

    Add basic PHP setup and test page for initial development

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>Test Page</h1>
+    <p>If you see this, the basic PHP setup is working.</p>
+</body>
+</html>
